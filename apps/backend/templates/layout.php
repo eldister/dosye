d0bfd7a0a8c7438f9476812dec112e4da01e585f
teban1812 <teban1812@d997aedd-4048-3087-1b1f-2401f1db9048>
2010-08-22 23:57:30 +0000

    Apariencia del backend

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -1,14 +1,41 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
+    <title>Módulo Administrativo de Dosye</title>
     <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
+    <?php use_stylesheet('admin.css') ?>
     <?php include_javascripts() ?>
+    <?php include_stylesheets() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div id="container">
+      <div id="header">
+        <h1>
+          <a href="<?php echo url_for('@homepage') ?>">
+            <img src="/images/jobeet.gif" alt="Jobeet Job Board" />
+          </a>
+        </h1>
+      </div>
+
+      <div id="menu">
+        <ul>
+          <li>
+            <?php echo link_to('Usuarios', 'guard/users') ?>
+          </li>
+          <li>
+            <?php echo link_to('Grupos', 'guard/groups') ?>
+          </li>
+        </ul>
+      </div>
+
+      <div id="content">
+        <?php echo $sf_content ?>
+      </div>
+
+      <div id="footer">
+          Dosye &copy; 2010 Technologicart
+      </div>
+    </div>
   </body>
 </html>
