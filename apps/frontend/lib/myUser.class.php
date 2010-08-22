33afa307e784cbe9bf6ed6ec5cc1f88ca954fd43
teban1812 <teban1812@d997aedd-4048-3087-1b1f-2401f1db9048>
2010-08-22 00:42:33 +0000

    configuración de vjCommentPlugin

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -2,4 +2,9 @@
 
 class myUser extends sfGuardSecurityUser
 {
+    public function setCulture($culture)
+    {
+        setlocale(LC_TIME, sfConfig::get('app_culture_locale'));
+        parent::setCulture($culture);
+    }
 }
