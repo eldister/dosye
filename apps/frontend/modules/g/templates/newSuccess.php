084b22bb786fa7b0377b43567f31ff56a51c1ba0
teban1812 <teban1812@d997aedd-4048-3087-1b1f-2401f1db9048>
2010-08-26 06:16:56 +0000

    bosquejos de los módulos person (p) y grouping (g)

---
+++ apps/frontend/modules/g/templates/newSuccess.php
@@ -0,0 +1,3 @@
+<h1>New Grouping</h1>
+
+<?php include_partial('form', array('form' => $form)) ?>
---
+++ apps/frontend/modules/p/templates/indexSuccess.php
@@ -0,0 +1,24 @@
+<h1>Persons List</h1>
+
+<table>
+  <thead>
+    <tr>
+      <th>Expediente</th>
+      <th>Nombre</th>
+      <th>Apellidos</th>
+      <th>Identificaci&oacute;n</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($persons as $person): ?>
+    <tr>
+      <td><a href="<?php echo url_for('p/show?id='.$person->getId()) ?>"><?php echo $person->getInternalId() ?></a></td>
+      <td><?php echo $person->getFirstName() ?></td>
+      <td><?php echo $person->getLastName() ?></td>
+      <td><?php echo $person->getIdentification() ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
+  <a href="<?php echo url_for('p/new') ?>">New</a>
