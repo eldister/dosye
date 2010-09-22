49c5a43f83c706eab0d6defb028bc00201437b09
teban1812 <teban1812@d997aedd-4048-3087-1b1f-2401f1db9048>
2010-09-22 21:01:49 +0000

    Página de inicio del frontend (port desde el prototipo)

---
+++ trunk/apps/frontend/modules/home/templates/viewSuccess.php
@@ -0,0 +1,27 @@
+	<div id="topcontent">
+	    <div class="wrap">
+
+		<h1 class="module_name">Inicio</h1>
+
+		<div class="clearfloat"></div>
+	    </div><!-- close .wrap -->
+	</div><!-- close #topcontent -->
+
+	<div id="content">
+	    <div class="wrap">
+
+		<div class="left_panel">
+
+                    <img src="<?php echo image_path('users.png') ?>" alt="folder" style="float:left" />
+		</div>
+		<div class="main_panel">
+		    <h3 class="content_title">&iexcl;La informaci&oacute;n de la gente PEA, en cualquier momento y en cualquier lugar!</h3>
+		    <p>Bienvenido al Sistema de Expediente Electr&oacute;nico del Programa de Educaci&oacute;n Abierta Vida Abundante.</p>
+		    <p>El sistema le permite tener acceso a los expedientes de voluntarios y estudiantes, desde cualquier lugar a trav&eacute;s de Internet.</p>
+		    <!--<p>Adem&aacute;s, este es un espacio de colaboraci&oacute;n para cada uno de los grupos de personas del PEA, donde se puede compartir archivos y anotaciones entre los miembros del grupo.</p>-->
+		</div>
+
+	       <div class="clearfloat"></div>
+
+	    </div><!-- close .wrap -->
+	</div><!-- close #content -->
