8122de4a741bf50145722c7f7ff2e7173f162e0f
teban1812 <teban1812@d997aedd-4048-3087-1b1f-2401f1db9048>
2010-10-07 03:53:13 +0000

    Ajustes a la configuración de las fechas

--- trunk/lib/form/doctrine/PersonFileForm.class.php
+++ trunk/lib/form/doctrine/PersonFileForm.class.php
@@ -12,26 +12,5 @@ class PersonFileForm extends BasePersonFileForm
 {
   public function configure()
   {
-    $format = '%day%/%month%/%year%';
-    $today = array(
-                'year'  => date('Y'),       
-                'month' => date('n'),
-                'day'   => date('j')
-	);
-	$birth_range = range(1900, date('Y'));
-    $birth_years = array_combine($birth_range,$birth_range);
-    
-    $pea_range = range(2000, date('Y'));
-    $pea_years = array_combine($pea_range,$pea_range);
-	
-    $this->widgetSchema['date_of_birth'] = new sfWidgetFormDate(array('format' => $format, 'years' => $birth_years));
-    $this->widgetSchema['pea_begin_date'] = new sfWidgetFormDate(array('format' => $format, 'default' => $today, 'years' => $pea_years));
-    $this->widgetSchema['pea_finish_date'] = new sfWidgetFormDate(array('format' => $format, 'years' => $pea_years));  
-    
-    $this->validatorSchema['date_of_birth'] = new sfValidatorDate(array('max' => date('Y-m-d')); 
-    $this->validatorSchema['pea_begin_date'] = new sfValidatorDate(array('max' => date('Y-m-d')); 
-    $this->validatorSchema['pea_finish_date'] = new sfValidatorDate(array('max' => date('Y-m-d')); 
-    
-    $this->validatorSchema->setPostValidator(new sfValidatorSchemaCompare('pea_begin_date', '<', 'pea_finish_date'));
   }
 }
