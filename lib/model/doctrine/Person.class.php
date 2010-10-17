4e03cf8ee3ffff08f6f42430d2390c0b5ae1bb9e
teban1812 <teban1812@d997aedd-4048-3087-1b1f-2401f1db9048>
2010-10-17 04:14:24 +0000

    Se cambia función de cálculo de la edad, debido a un warning que se estuvo mostrando en frontend/p/show: range "step exceeds the specified range"

--- lib/model/doctrine/Person.class.php
+++ lib/model/doctrine/Person.class.php
@@ -10,17 +10,20 @@
  * @author     Your name here
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
-class Person extends BasePerson
-{
-    public function getFullName()
-    {
-        return $this->getFirstName().' '.$this->getLastName();
+class Person extends BasePerson {
+
+    public function getFullName() {
+        return $this->getFirstName() . ' ' . $this->getLastName();
     }
 
-    public function getAge()
-    {
-        $today = new DateTime();
-        $dateOfBirth = new DateTime($this->getDateOfBirth());
-        return $today->diff($dateOfBirth)->y;
+    public function getAge() {
+        list($year, $month, $day) = explode("-", $this->getDateOfBirth());
+        $year_diff = date("Y") - $year;
+        $month_diff = date("m") - $month;
+        $day_diff = date("d") - $day;
+        if ($day_diff < 0 || $month_diff < 0)
+            $year_diff--;
+        return $year_diff;
     }
+
 }
