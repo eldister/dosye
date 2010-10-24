f63b3957d1fb4da0a1e805be92a1d30a55152d74
teban1812 <teban1812@d997aedd-4048-3087-1b1f-2401f1db9048>
2010-10-24 16:15:10 +0000

    Se agrega archivos de traducción

--- apps/backend/modules/sfGuardAuth/templates/secureSuccess.php
+++
@@ -1,9 +0,0 @@
-<?php use_helper('I18N') ?>
-
-<h2><?php echo __('La página que pides está asegurada y no tienes los suficientes privilegios para accederla.', null, 'sf_guard') ?></h2>
-
-<p><?php echo sfContext::getInstance()->getRequest()->getUri() ?></p>
-
-<h3><?php echo __('Ingrese al sistema para obtener acceso.', null, 'sf_guard') ?></h3>
-
-<?php echo get_component('sfGuardAuth', 'signin_form') ?>
